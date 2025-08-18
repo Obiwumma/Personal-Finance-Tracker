c401be068dc57d5a60ea23c2977c9fc276ef53df
Obiwumma Pascal <pobiwumma@gmail.com>
2025-08-18 16:30:42 +0100

    put dropdown category in its own s

--- personal-finance-tracker/src/App.jsx
+++ personal-finance-tracker/src/App.jsx
@@ -2,43 +2,27 @@ import { useState } from 'react'
 import './App.css'
 
 
-function App() {
-  const [dropdown, setDropdown] = useState(false)
-  const [selected, setSelected] = useState('All')
+import Category from './components/category'
 
-  function handleclick() {
-    setDropdown(!dropdown)
-  }
+function App() {
+  
 
 
   return (
     <>
-    <div className='m-4 p-4 bg-[#b0b4b9] flex-col items-center justify-center h-svh '>
+    <div className='m-4 p-4 bg-[#b0b4b9] flex-col items-center justify-center h-svh font-sans '>
       <header>
-        <h1 className='font-sans font-semibold text-3xl'>Finance Tracker</h1>
+        <h1 className=' font-semibold text-3xl'>Finance Tracker</h1>
         <div><p className='text-2xl font-semibold '>$2,000.00</p></div>
       </header>
 
-      <div className="relative bg-white border rounded-lg ">
-        {/* <!-- Fake select button --> */}
-        <button onClick={handleclick} class=" w-full px-4 py-2 bg-[#b0b4b9] border rounded">
-          All
-          <span className="float-right">▼</span>
-        </button>
-        
-        {/* <!-- Dropdown options --> */}
-        {
-          dropdown ? 
-          <div className="absolute  w-full bg-white border rounded shadow-lg">
-            <div className="p-2 hover:bg-blue-100">some</div>
-            <div className="p-2 hover:bg-blue-100">few</div>
-          </div> : null
-        }
-      </div>
+      <>
+        <Category/>
+      </>
 
       <main>
-        <div>Transaction</div>
-        <div>{/* Transaction list */}</div>
+        <div className='text-2xl font-semibold'>Transaction</div>
+        <div className='bg-white h-44 border rounded-lg'>{/* Transaction list */}</div>
       </main>
 
     </div>
---
+++ personal-finance-tracker/src/components/category.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+function Category() {
+  const [dropdown, setDropdown] = useState(false)
+  const [selected, setSelected] = useState('All')
+
+  function handleCategory() {
+    setDropdown(!dropdown)
+  }
+
+  function handleSome() {
+    setSelected("some")
+    setDropdown(!dropdown)
+  }
+
+  function handleFew() {
+    setSelected("Few")
+    setDropdown(!dropdown)
+  }
+
+  function handleAll() {
+    setSelected("All")
+    setDropdown(!dropdown)
+  }
+
+  return <>
+    <div className="relative bg-white border rounded-lg ">
+        {/* <!-- Fake select button --> */}
+        <button onClick={handleCategory} class=" w-full px-4 py-2 bg-[#b0b4b9] border rounded">
+          {selected}
+          <span className="float-right">▼</span>
+        </button>
+        
+        {/* <!-- Dropdown options --> */}
+        {
+          dropdown ? 
+          <div className="absolute  w-full bg-white border rounded shadow-lg">
+            <div onClick={handleAll} className="p-2 hover:bg-blue-100">All</div>
+            <div onClick={handleSome} className="p-2 hover:bg-blue-100">Some</div>
+            <div onClick={handleFew}  className="p-2 hover:bg-blue-100">Few</div>
+          </div> : null
+        }
+      </div>
+  </>
+}
+
+export default Category;
