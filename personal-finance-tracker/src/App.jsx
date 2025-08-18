284eb0067bea984521549c546e63fab12d4da3e4
Obiwumma Pascal <pobiwumma@gmail.com>
2025-08-18 15:48:25 +0100

    Added drop down functionality

--- personal-finance-tracker/src/App.jsx
+++ personal-finance-tracker/src/App.jsx
@@ -1,17 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-// const dropdownBtn = document.getElementById('dropdownBtn')
-// const dropdownMenu = document.getElementById('dropdownMenu')
-
-// dropdownBtn.addEventListener('click' , function() {
-//   dropdownMenu.classList.toggle('hidden')
-// })
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [dropdown, setDropdown] = useState(false)
+  const [selected, setSelected] = useState('All')
+
+  function handleclick() {
+    setDropdown(!dropdown)
+  }
+
 
   return (
     <>
@@ -21,18 +19,21 @@ function App() {
         <div><p className='text-2xl font-semibold '>$2,000.00</p></div>
       </header>
 
-      <div className="relative bg-white border rounded-lg p-2">
+      <div className="relative bg-white border rounded-lg ">
         {/* <!-- Fake select button --> */}
-        <button id='dropdownBtn' class=" w-full px-4 py-2 bg-[#b0b4b9] border rounded">
+        <button onClick={handleclick} class=" w-full px-4 py-2 bg-[#b0b4b9] border rounded">
           All
           <span className="float-right">▼</span>
         </button>
         
         {/* <!-- Dropdown options --> */}
-        <div id='dropdownMenu' className="absolute hidden w-full bg-white border rounded shadow-lg">
-          <div className="p-2 hover:bg-blue-100">some</div>
-          <div className="p-2 hover:bg-blue-100">few</div>
-        </div>
+        {
+          dropdown ? 
+          <div className="absolute  w-full bg-white border rounded shadow-lg">
+            <div className="p-2 hover:bg-blue-100">some</div>
+            <div className="p-2 hover:bg-blue-100">few</div>
+          </div> : null
+        }
       </div>
 
       <main>
