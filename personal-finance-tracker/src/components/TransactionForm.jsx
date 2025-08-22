a7dd5d264b251ebac3c6a932191ab87d7647ef63
Obiwumma Pascal <pobiwumma@gmail.com>
2025-08-22 20:05:25 +0100

    Used React hook form for form validation

--- personal-finance-tracker/src/components/TransactionForm.jsx
+++ personal-finance-tracker/src/components/TransactionForm.jsx
@@ -1,9 +1,88 @@
+import { useState } from "react";
+import { useForm } from 'react-hook-form';
+
 function TransactionForm() {
+
+      const { register, handleSubmit, formState: { errors }, reset } = useForm();
+
+
+
+  const onHandleSubmit = (data) => {
+        console.log(data)
+        console.log(errors)
+        reset();
+      };
   
   return(
     <>
-      <div className="bg-red-400">
-        happy
+      <div className="">
+        <form onSubmit={handleSubmit(onHandleSubmit)} className="space-y-4">
+          {/* Description */}
+      <div>
+        <label>Description</label>
+        <input
+          type="text"
+          {...register("description", { required: "Description is required" })}
+          className="border p-2 w-full"
+        />
+        {errors.description && (
+          <p className="text-red-500 text-sm italic">{errors.description.message}</p>
+        )}
+      </div>
+
+
+          {/* Amount */}
+      <div>
+        <label>Amount</label>
+        <input
+          type="number"
+          {...register("amount", {
+            required: "Amount is required",
+            min: { value: 1, message: "Amount must be greater than 0" },
+          })}
+          className="border p-2 w-full"
+        />
+        {errors.amount && (
+          <p className="text-red-500 text-sm italic">{errors.amount.message}</p>
+        )}
+      </div>
+
+
+          {/* Category */}
+      <div>
+        <label>Category</label>
+        <select
+          {...register("category", { required: "Category is required" })}
+          className="border p-2 w-full"
+        >
+          <option value="">-- Select Category --</option>
+          <option value="income">Income</option>
+          <option value="expenses">Expenses</option>
+        </select>
+        {errors.category && (
+          <p className="text-red-500 text-sm italic">{errors.category.message}</p>
+        )}
+      </div>
+
+
+          {/* Date */}
+      <div>
+        <label>Date</label>
+        <input
+          type="date"
+          {...register("date", { required: "Date is required" })}
+          className="border p-2 w-full"
+        />
+        {errors.date && (
+          <p className="text-red-500 text-sm italic">{errors.date.message}</p>
+        )}
+      </div>
+
+
+          <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded" >
+            SUBMIT
+          </button>
+        </form>
       </div>
     </>
   )
