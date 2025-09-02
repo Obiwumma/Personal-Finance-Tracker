972ad21a74c429f69f666548f68470093fb887a2
Obiwumma Pascal <pobiwumma@gmail.com>
2025-09-02 20:02:13 +0100

    integrated a back LINK

--- personal-finance-tracker/src/components/Statistics.jsx
+++ personal-finance-tracker/src/components/Statistics.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import PieChartStat from './PieChartStat';
 import BarChartStat from './BarChartStat';
+import { Link } from "react-router-dom";
 
 function Statistics() {
 
   
   return (
     <div>  
+      <header>
+        <Link to='/'>Back</Link>
+      </header>
       <BarChartStat/>
       <PieChartStat/>
     </div>
