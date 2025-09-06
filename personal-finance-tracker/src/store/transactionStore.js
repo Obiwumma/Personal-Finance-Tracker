def71187d88dc50e32589930489e4749dc8d1106
Obiwumma Pascal <pobiwumma@gmail.com>
2025-09-06 22:31:42 +0100

    Added data persist to the transaction store

--- personal-finance-tracker/src/store/transactionStore.js
+++ personal-finance-tracker/src/store/transactionStore.js
@@ -1,28 +1,39 @@
 import { create } from "zustand"
+import { persist } from "zustand/middleware"
 
-export const useTransactionStore = create((set, get) => ({
-  //State
-  transactions: [],
-  filterCategory: 'all',
+export const useTransactionStore = create(
+  persist(
+    (set, get) => ({
+      //State
+      transactions: [],
+      filterCategory: 'all',
 
-  // Actions
-  addTransaction: (transaction) => 
-    set((state) => ({
-    transactions:[...state.transactions, transaction]
-  })) ,
-
-
-  deleteTransaction: (id) => 
-    set((state) => ({
-    transactions: state.transactions.filter((transaction) => transaction.id !== id)
-  })),
-
-  setFilterCategory: (category) => set({ filterCategory: category }),
-
-  
+      // Actions
+      addTransaction: (transaction) => 
+        set((state) => ({
+        transactions:[...state.transactions, transaction]
+      })) ,
 
 
-}))
+      deleteTransaction: (id) => 
+        set((state) => ({
+        transactions: state.transactions.filter((transaction) => transaction.id !== id)
+      })),
+
+      setFilterCategory: (category) => set({ filterCategory: category }),
+      
+    }),
+
+    {
+      name: "transactions-storage", // key in localStorage
+      partialize: (state) => ({
+        transactions: state.transactions,
+        filters: state.filters,
+      }),
+    }
+
+  )
+)
 
 
 
