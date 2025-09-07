951b00e20f28d5103362056f6fffde1cff376f4b
Obiwumma Pascal <pobiwumma@gmail.com>
2025-09-07 21:51:27 +0100

    Added Category btn

--- personal-finance-tracker/src/components/category.jsx
+++ personal-finance-tracker/src/components/category.jsx
@@ -2,17 +2,14 @@ import { useState } from "react";
 import { useTransactionStore } from "../store/transactionStore";
 
 function Category() {
-  const { setFilterCategory } = useTransactionStore();
-
-  // Now compute filteredTransactions locally
-  
+  const { filterCategory, setFilterCategory } = useTransactionStore();
   
 
   return <>
     <div className="flex gap-4 bg-[#ffffff] border rounded-lg p-2 overflow-auto md:w-[50%] md:items-center md:justify-evenly ">
-        <button className="category-btn" onClick={() => setFilterCategory("all")}>All</button>
-        <button className="category-btn" onClick={() => setFilterCategory("Expenses")}>Expenses</button>
-        <button className="category-btn" onClick={() => setFilterCategory("Income")}>Income</button>
+        <button className={`category-btn ${filterCategory == "all" ? "bg-gray-800 text-white hover:bg-gray-600" : "bg-[#eeeeee] hover:bg-gray-300"}` } onClick={() => setFilterCategory("all")}>All</button>
+        <button className={`category-btn ${filterCategory == "Expenses" ? "bg-gray-800 text-white hover:bg-gray-600" : "bg-[#eeeeee] hover:bg-gray-300"}` } onClick={() => setFilterCategory("Expenses")}>Expenses</button>
+        <button className={`category-btn ${filterCategory == "Income" ? "bg-gray-800 text-white hover:bg-gray-600" : "bg-[#eeeeee] hover:bg-gray-300"}` } onClick={() => setFilterCategory("Income")}>Income</button>
       </div>
   </>
 }
