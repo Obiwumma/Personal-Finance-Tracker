ba5df8c85e33c3cf8071d71b85c4f867128c0044
Obiwumma Pascal <pobiwumma@gmail.com>
2025-09-07 21:06:34 +0100

    Adjusted ADD button

--- personal-finance-tracker/src/components/Layout.jsx
+++ personal-finance-tracker/src/components/Layout.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
 import { useState } from 'react'
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+/* import all the icons in Free Solid, Free Regular, and Brands styles */
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+library.add(fas, far, fab)
+
 import Modal from './Modal'
 import Category from './category'
 import TransactionList from './List'
@@ -30,7 +39,9 @@ function Layout() {
               <TransactionList />
             </main>
 
-            <button className='bg-green-600 hover:bg-green-700 h-16 w-16 fixed bottom-6 right-6 rounded-full flex items-center justify-center text-5xl text-white shadow-lg transition' onClick={() => setIsOpen(true)} > + </button>
+            <button className='bg-green-600 hover:bg-green-700 fixed bottom-6 right-6 rounded-full flex items-center justify-center text-4xl text-white h-15 w-15 shadow-lg transition' onClick={() => setIsOpen(true)} >
+              <FontAwesomeIcon icon="fa-solid fa-plus" />
+            </button>
 
           </div>
         </div>
